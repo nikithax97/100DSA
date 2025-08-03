b75bf3871ed2e18fe49b22aff840fc0473041288
nikithax97 <sainikitha71z@gmail.com>
2025-08-03 21:11:29 +0530

    Create day11.js

---
+++ day11.js
@@ -0,0 +1,35 @@
+//DAY-11  
+//Find All Unique Triplets That Sum to Zero
+const arr = [-1, 0, 1, 2, -1, -4];
+
+function triplet(arr) {
+    arr.sort((a,b) => a - b);
+    const result = [];
+
+    for(let i = 0; i < arr.length - 2; i++) {
+        if(i > 0 && arr[i] === arr[i - 1]) continue;
+
+        let left = i + 1;
+        let right = arr.length -1;
+
+        while(left < right) {
+            const sum = arr[i] + arr[left] + arr[right];
+
+            if(sum === 0) {
+                result.push([arr[i], arr[left], arr[right]]);
+               //skips duplicates for left and right
+                while(left < right && arr[left] === arr[left + 1]) left++;
+                while(left < right && arr[right] === arr[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if(sum < 0) {
+                left++;
+            } else {
+                right--;
+            } 
+        }
+    }
+    return result;
+} 
+console.log(triplet(arr))
